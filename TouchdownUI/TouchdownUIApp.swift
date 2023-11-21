290d9c344ba9e7b34af6147adaea5b328952b289
Daniel Washington Ignacio <danielvertigo@hotmail.com>
2023-11-21 15:13:21 -0300

    Initial Commit

---
+++ TouchdownUI/TouchdownUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownUIApp.swift
+//  TouchdownUI
+//
+//  Created by Daniel Washington Ignacio on 21/11/23.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
